e69b452c7c8e9aa860294ccdb95cfb0d1f408eb2
CRGBS <32331418+CRGBS@users.noreply.github.com>
2023-05-13 22:17:30 +0800

    Update BOT.py
    optimz

--- BOT.py
+++ BOT.py
@@ -11,11 +11,13 @@ client = discord.Client(intents=intents)
 CHANNEL_ID = INPUTURCHANNELID
 BOT_TOKEN = "INPUTURTOKEN"
 
+collected_text = ''
 process = None
+
 async def open_exe_file():
-    global proces
-    s#edit to ur llamacpp location
-    command = "J:/GPTAI/llamacpp/main.exe -m zh-models/13B/ggml-model-q5_0.bin --color -f prompts/alpaca.txt -ins -c 2048 --temp 0.2 -n 256 -t 16 --repeat_penalty 1.1 --temp 0.4"
+    global process, collected_text
+    # edit to your llamacpp location
+    command = "J:/GPTAI/llamacpp/main.exe -m zh-models/13B/ggml-model-q5_0.bin -f prompts/alpaca.txt -ins -c 2048 --temp 0.2 -n 256 -t 7 --top_k 40 --top_p 0.5 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1.17647 --temp 0.7"
     process = await asyncio.create_subprocess_shell(
         command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE,
         stdin=asyncio.subprocess.PIPE, shell=True, env=os.environ
@@ -28,41 +30,45 @@ async def open_exe_file():
 
         output = await process.stdout.read(1024)
         if output:
-            decoded_output = output.decode('big5', errors='replace').rstrip()
+            decoded_output = output.decode('utf-8', errors='replace').rstrip()
             if decoded_output.strip():
-                # 移除 ansi 顏色代碼
-                ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
-                plain_text_output = ansi_escape.sub('', decoded_output)
-                print(plain_text_output)
-                if plain_text_output.strip():
-                    await client.get_channel(CHANNEL_ID).send(plain_text_output)
+                await client.get_channel(CHANNEL_ID).send(decoded_output)
+                print(decoded_output)
+        
+                # 判斷訊息是否為第一次輸入
+                if collected_text == '':
+                    collected_text = decoded_output
+                    await client.get_channel(CHANNEL_ID).send(f'F已蒐集到：{collected_text}')
+                else:
+                    collected_text += decoded_output
+                    # 編輯前一則訊息
+                    async for m in client.get_channel(CHANNEL_ID).history(limit=1):
+                        await m.edit(content=f'N已蒐集到：{collected_text}')
+                    async for m in client.get_channel(CHANNEL_ID).history(limit=2):
+                        await m.delete()
 
-    # 關閉標準輸出
-    if process.stdout:
-        process.stdout.close()
-
-client = discord.Client(intents=discord.Intents.all())
+async def send_command(cmd):
+    global process, collected_text
+    if process is None:
+        await open_exe_file()
+    if process.returncode is None:
+        process.stdin.write((cmd + '\n').encode('utf-8'))
+        print(f'Received command: {cmd}')
+        await process.stdin.drain()
+        collected_text = ''
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if client.user.mentioned_in(message):
-        await message.channel.send('hello，bot loading')
+        await message.channel.send('Let Me Think 🤔')
         cmd = message.content.split(f'<@{client.user.id}>')[1].strip()
-        print(f'Received command: {cmd}')
-        if process is not None:
-            process.stdin.write(cmd.encode('utf-8') + b'\n')
-            await process.stdin.drain()
+        await send_command(cmd)
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
-    #global process
-    #if process is None:
     await open_exe_file()
-    #else:
-    #    print('Exe file is opened:', process is not None)
-    #asyncio.create_task(read_stdout())
 
 client.run(BOT_TOKEN)
