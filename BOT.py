a0347dfc0b0a333150a69ab32968e9924e63ff37
CRGBS <32331418+CRGBS@users.noreply.github.com>
2023-05-14 09:08:14 +0800

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -2,22 +2,27 @@ import subprocess
 import asyncio
 import discord
 import os
-import re
+
+###EDIT IT
+CHANNEL_ID = INPUTURCHANNELID
+BOT_TOKEN = "INPUTURTOKEN"
+ROLE = 'inputurrole'
+###
 
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
 client = discord.Client(intents=intents)
-CHANNEL_ID = INPUTURCHANNELID
-BOT_TOKEN = "INPUTURTOKEN"
 
+# 定義變數，用來紀錄蒐集到的文字
 collected_text = ''
 process = None
+msgr = None   # 声明变量 msgr 并初始化为 None 对象
 
 async def open_exe_file():
     global process, collected_text
     # edit to your llamacpp location
-    command = "J:/GPTAI/llamacpp/main.exe -m zh-models/13B/ggml-model-q5_0.bin -f prompts/alpaca.txt -ins -c 2048 --temp 0.2 -n 256 -t 7 --top_k 40 --top_p 0.5 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1.17647 --temp 0.7"
+    command = "title MH8BOT & J:/GPTAI/llamacpp/main.exe -m zh-models/13B/ggml-model-q5_0.bin -f prompts/alpaca.txt -ins -c 2048 --temp 0.2 -n 256 -t 7 --top_k 40 --top_p 0.5 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1.17647 --temp 0.68"
     process = await asyncio.create_subprocess_shell(
         command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE,
         stdin=asyncio.subprocess.PIPE, shell=True, env=os.environ
@@ -32,20 +37,16 @@ async def open_exe_file():
         if output:
             decoded_output = output.decode('utf-8', errors='replace').rstrip()
             if decoded_output.strip():
-                await client.get_channel(CHANNEL_ID).send(decoded_output)
+                #await client.get_channel(CHANNEL_ID).send(f'ER：{decoded_output}')
                 print(decoded_output)
-        
+
                 # 判斷訊息是否為第一次輸入
                 if collected_text == '':
                     collected_text = decoded_output
-                    await client.get_channel(CHANNEL_ID).send(f'F已蒐集到：{collected_text}')
+                    msgr = await client.get_channel(CHANNEL_ID).send(f'FR：{collected_text}')
                 else:
                     collected_text += decoded_output
-                    # 編輯前一則訊息
-                    async for m in client.get_channel(CHANNEL_ID).history(limit=1):
-                        await m.edit(content=f'N已蒐集到：{collected_text}')
-                    async for m in client.get_channel(CHANNEL_ID).history(limit=2):
-                        await m.delete()
+                    await msgr.edit(content=f'NR：{collected_text}')
 
 async def send_command(cmd):
     global process, collected_text
@@ -56,10 +57,14 @@ async def send_command(cmd):
         print(f'Received command: {cmd}')
         await process.stdin.drain()
         collected_text = ''
+        msgr = ''
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
+        return
+    if not any(role.name == ROLE for role in message.author.roles):
+        await message.channel.send('Permission Denied!!')
         return
     if client.user.mentioned_in(message):
         await message.channel.send('Let Me Think 🤔')
