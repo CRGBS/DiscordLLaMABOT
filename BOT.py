1960986b2d3df0859896f870bf269a8296305653
CRGBS <32331418+CRGBS@users.noreply.github.com>
2023-05-14 22:38:47 +0800

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -22,10 +22,10 @@ msgr = None   # 声明变量 msgr 并初始化为 None 对象
 async def open_exe_file():
     global process, collected_text
     # edit to your llamacpp location
-    command = "chcp 65001 & title MH8BOT & J:/GPTAI/llamacpp/main.exe -m zh-models/13B/ggml-model-q5_0.bin -f prompts/alpaca.txt -ins -c 2048 --temp 0.2 -n 256 -t 7 --top_k 40 --top_p 0.5 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1.17647 --temp 0.68"
+    command = "title MH8BOT & J:/GPTAI/llamacpp/main.exe -m zh-models/13B/ggml-model-q5_0.bin -f prompts/alpaca.txt -ins -c 2048 --temp 0.2 -n 256 -t 7 --top_k 40 --top_p 0.5 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1.17647 --temp 0.68"
     process = await asyncio.create_subprocess_shell(
         command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE,
-        stdin=asyncio.subprocess.PIPE, shell=True, env=os.environ
+        stdin=asyncio.subprocess.PIPE, creationflags=subprocess.CREATE_NEW_CONSOLE
     )
 
     while True:
@@ -33,11 +33,10 @@ async def open_exe_file():
         if process.returncode is not None:
             break
 
-        output = await process.stdout.read(1024)
+        output = await process.stdout.read(512)
         if output:
             decoded_output = output.decode('utf-8', errors='replace').rstrip()
             if decoded_output.strip():
-                #await client.get_channel(CHANNEL_ID).send(f'ER：{decoded_output}')
                 print(decoded_output)
 
                 # 判斷訊息是否為第一次輸入
@@ -47,14 +46,15 @@ async def open_exe_file():
                 else:
                     collected_text += decoded_output
                     await msgr.edit(content=f'NR：{collected_text}')
-
+                    
 async def send_command(cmd):
     global process, collected_text
     if process is None:
         await open_exe_file()
     if process.returncode is None:
-        process.stdin.write((cmd + '\n').encode('utf-8'))
-        print(f'Received command: {cmd}')
+        intext = cmd.encode('utf-8')
+        process.stdin.write(intext)
+        print(f'Received command: {intext}')
         await process.stdin.drain()
         collected_text = ''
         msgr = ''
@@ -65,7 +65,7 @@ async def on_message(message):
         return
     if client.user.mentioned_in(message):
         if not any(role.name == ROLE for role in message.author.roles):
-            await message.channel.send('Permission Denied!!')
+            await message.channel.send('只有至高無上的月球指名能指使我做事!!')
             return
         else:
             await message.channel.send('Let Me Think 🤔')
