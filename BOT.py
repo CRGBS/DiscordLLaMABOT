5c27534e2058e9d38032ca67bfeda804e1bc094b
CRGBS <32331418+CRGBS@users.noreply.github.com>
2023-05-16 18:01:01 +0800

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -16,11 +16,10 @@ client = discord.Client(intents=intents)
 
 # 定義變數
 collected_text, process, msgr = None, None, None
-#TODO 讓中文能被正確交互
 async def open_exe_file():
     global process, collected_text
     # edit to your llamacpp location
-    command = "title MH8BOT & J:/GPTAI/llamacpp/main.exe -m zh-models/13B/ggml-model-q5_0.bin -f prompts/alpaca.txt -ins -c 2048 --keep 2048 -n 512 -t 7 --mlock --memory_f32 --top_k 40 --top_p 0.53 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1.17647 --temp 0.667"
+    command = "title MH8BOT & J:/GPTAI/llamacpp/mainMH8.exe -m zh-models/13B/ggml-model-q5_0.bin -f prompts/alpaca.txt -ins"
     process = await asyncio.create_subprocess_shell(
         command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE,
         stdin=asyncio.subprocess.PIPE
@@ -35,15 +34,22 @@ async def open_exe_file():
         if output:
             decoded_output = output.decode('utf-8', errors='replace')#.rstrip()
             if decoded_output.strip():
-                print(decoded_output)
+                async with client.get_channel(CHANNEL_ID).typing():
+                    print(decoded_output)
 
-                # 判斷訊息是否為第一次輸入
-                if collected_text is None:
-                    collected_text = decoded_output
-                    msgr = await client.get_channel(CHANNEL_ID).send(f'Replay：{collected_text}')
-                else:
-                    collected_text += decoded_output
-                    await msgr.edit(content=f'Reply：{collected_text}')
+                    # 判斷訊息是否為第一次輸入
+                    if collected_text is None:
+                        collected_text = decoded_output
+                        embed = discord.Embed(title="Thinking..", description=collected_text)
+                        msgr = await client.get_channel(CHANNEL_ID).send(embed=embed)
+                    
+                        #msgr = await client.get_channel(CHANNEL_ID).send(f'Replay：{collected_text}')
+                    else:
+                        collected_text += decoded_output
+                        embed = discord.Embed(title="answer:", description=collected_text)
+                        await msgr.edit(embed=embed)
+                    
+                        #await msgr.edit(content=f'Reply：{collected_text}')
                     
 
 async def send_command(cmd):
@@ -51,9 +57,10 @@ async def send_command(cmd):
     if process is None:
         await open_exe_file()
     if process.returncode is None:
-        intext = cmd.encode('utf-8') + b'/n'
+        question = cmd
+        intext = cmd.encode('utf-8') + b'\n'
         process.stdin.write(intext)
-        print(f"Received command: {intext}")
+        print(f"cmd: {cmd}")
         await process.stdin.drain()
         collected_text, msgr = None, None
 
@@ -66,7 +73,7 @@ async def on_message(message):
             await message.channel.send('Role Denied!!')
             return
         else:
-            await message.channel.send('Let Me Think 🤔')
+            await message.channel.send('Let me thing.. 🤔')
             cmd = message.content.split(f'<@{client.user.id}>')[1].strip()
             await send_command(cmd)
 
